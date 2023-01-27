01c9608bfebc3b1a2e209b6818be68f9fe33417b
Nikolaka17 <92060768+Nikolaka17@users.noreply.github.com>
2023-01-27 09:43:05 -0600

    Create City.java

---
+++ City.java
@@ -0,0 +1,20 @@
+import java.awt.Polygon;
+
+public class City extends Polygon{
+    
+    private int length;
+    
+    public City(int x, int y, int l){
+        length = l;
+        npoints = 18;
+        ypoints = new int[npoints];
+        xpoints = new int[npoints];
+        
+        int[][] offsets = new int[][]{{0, 8}, {0, 6}, {2, 6}, {2, 2}, {4, 2}, {4, 0}, {6, 0}, {6, 2}, {8, 2}, {8, 4}, {10, 4}, {10, 1}, {12, 1}, {12, 0}, {14, 0}, {14, 6}, {16, 6}, {16, 8}};
+        
+        for(int i = 0; i < npoints; i++){
+            xpoints[i] = x + (l * offsets[i][0]);
+            ypoints[i] = y + (l * offsets[i][1]);
+        }
+    }
+}
