01c1f7948f42ade2c72ed44b2a787931ffde26a7
Nikolaka17 <92060768+Nikolaka17@users.noreply.github.com>
2023-02-02 09:52:16 -0600

    Update MCWindow.java

--- MCWindow.java
+++ MCWindow.java
@@ -1,8 +1,10 @@
 import javax.swing.JPanel;
+import javax.swing.ImageIcon;
 
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.Color;
+import java.awt.Polygon;
 
 import java.util.ArrayList;
 
@@ -18,6 +20,12 @@ public class MCWindow extends JPanel{
     public void paintComponent(Graphics g){
         super.paintComponent(g);
         Graphics2D g2 = (Graphics2D) g;
+        
+        //g2.drawImage(new ImageIcon("missile.jpg").getImage(), 0, 0, getWidth(), getHeight(), null);
+        
+        g2.setColor(Color.GREEN);
+        int[][] offsets = new int[][]{{9, 57, 98, 163, 221, 339, 371, 447, 482, 630, 662, 723, 782},{534, 534, 480, 480, 530, 530, 480, 480, 530, 530, 480, 480, 530}};
+        g2.fill(new Polygon(offsets[0], offsets[1], offsets[0].length));
 
         g2.setColor(Color.BLUE);
         for(City c: cities){
