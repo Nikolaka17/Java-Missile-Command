04f977be743fe8402eeb94adf9f485a06cf78a69
Nikolaka17 <92060768+Nikolaka17@users.noreply.github.com>
2023-02-02 09:52:41 -0600

    Update MissileCommand.java

--- MissileCommand.java
+++ MissileCommand.java
@@ -1,4 +1,6 @@
 import javax.swing.JFrame;
+import java.awt.event.MouseListener;
+import java.awt.event.MouseEvent;
 
 public class MissileCommand {
     
@@ -6,6 +8,29 @@ public class MissileCommand {
         JFrame window = new JFrame();
         MCWindow panel = new MCWindow();
 
+        window.addMouseListener(new MouseListener(){
+            @Override
+            public void mouseClicked(MouseEvent me){
+                System.out.println("(" + me.getX() + ", "+ me.getY() + ")");
+            }
+
+            @Override
+            public void mousePressed(MouseEvent me) {
+                
+            }
+
+            @Override
+            public void mouseReleased(MouseEvent me){
+            }
+
+            @Override
+            public void mouseEntered(MouseEvent me){
+            }
+
+            @Override
+            public void mouseExited(MouseEvent me){
+            }
+        });
         window.add(panel);
 
         window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
