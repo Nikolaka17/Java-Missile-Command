858018f8a6bd57d8295aca01293264e91957f3dd
Nikolaka17 <92060768+Nikolaka17@users.noreply.github.com>
2023-02-05 18:44:47 -0600

    Update MCWindow.java

--- MCWindow.java
+++ MCWindow.java
@@ -15,7 +15,6 @@ public class MCWindow extends JPanel{
     public MCWindow(){
         super();
         setBackground(Color.BLACK);
-        setup();
     }
 
     public void setup(){
@@ -25,18 +24,25 @@ public class MCWindow extends JPanel{
                 cities.remove(c);
             }
         }
+        int w = getWidth();
+        int h = getHeight();
+        System.out.println(h);
+        cities.add(new City(w/5,200, w/192));
+        cities.add(new City(w/5 + w/12, 200, w/192));
     }
 
     @Override
     public void paintComponent(Graphics g){
         super.paintComponent(g);
         Graphics2D g2 = (Graphics2D) g;
-        
-        g2.drawImage(new ImageIcon("missile.jpg").getImage(), 0, 0, getWidth(), getHeight(), null);
-        
-        g2.setColor(Color.GREEN);
+
         int w = getWidth();
         int h = getHeight();
+        setup();
+        
+        g2.drawImage(new ImageIcon("missile.jpg").getImage(), 0, 0, w, h, null);
+        
+        g2.setColor(Color.GREEN);
         int[][] offsets = new int[][]{{0, 0, w/20, w/10, 3*w/20, w/5, 9*w/20, w/2, 11*w/20, 12*w/20, 17*w/20, 18*w/20, 19*w/20, w, w},{h, h-(h/10), h-(h/10), h-(h/5), h-(h/5), h-(h/10), h-(h/10), h-(h/5), h-(h/5), h-(h/10), h-(h/10), h-(h/5), h-(h/5), h-(h/10), h}};
         g2.fill(new Polygon(offsets[0], offsets[1], offsets[0].length));
 
