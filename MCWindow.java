16f5436bad9641335f821a725b3048d169cfe58a
Nikolaka17 <92060768+Nikolaka17@users.noreply.github.com>
2023-02-05 18:27:52 -0600

    Update MCWindow.java

--- MCWindow.java
+++ MCWindow.java
@@ -10,10 +10,21 @@ import java.util.ArrayList;
 
 public class MCWindow extends JPanel{
     private ArrayList<City> cities = new ArrayList<City>();
+    private int[] missiles = new int[]{10, 10, 10};
     
     public MCWindow(){
         super();
         setBackground(Color.BLACK);
+        setup();
+    }
+
+    public void setup(){
+        missiles = new int[]{10, 10, 10};
+        if(cities.size() != 0){
+            for(City c: cities){
+                cities.remove(c);
+            }
+        }
     }
 
     @Override
@@ -21,10 +32,12 @@ public class MCWindow extends JPanel{
         super.paintComponent(g);
         Graphics2D g2 = (Graphics2D) g;
         
-        //g2.drawImage(new ImageIcon("missile.jpg").getImage(), 0, 0, getWidth(), getHeight(), null);
+        g2.drawImage(new ImageIcon("missile.jpg").getImage(), 0, 0, getWidth(), getHeight(), null);
         
         g2.setColor(Color.GREEN);
-        int[][] offsets = new int[][]{{9, 57, 98, 163, 221, 339, 371, 447, 482, 630, 662, 723, 782},{534, 534, 480, 480, 530, 530, 480, 480, 530, 530, 480, 480, 530}};
+        int w = getWidth();
+        int h = getHeight();
+        int[][] offsets = new int[][]{{0, 0, w/20, w/10, 3*w/20, w/5, 9*w/20, w/2, 11*w/20, 12*w/20, 17*w/20, 18*w/20, 19*w/20, w, w},{h, h-(h/10), h-(h/10), h-(h/5), h-(h/5), h-(h/10), h-(h/10), h-(h/5), h-(h/5), h-(h/10), h-(h/10), h-(h/5), h-(h/5), h-(h/10), h}};
         g2.fill(new Polygon(offsets[0], offsets[1], offsets[0].length));
 
         g2.setColor(Color.BLUE);
