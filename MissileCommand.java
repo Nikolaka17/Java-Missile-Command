6b7397e0657c4d01c97d21b0c8eafdb0b4e9a390
Nikolaka17 <92060768+Nikolaka17@users.noreply.github.com>
2023-03-08 15:24:08 -0600

    Update MissileCommand.java

--- MissileCommand.java
+++ MissileCommand.java
@@ -13,11 +13,12 @@ public class MissileCommand {
         Timer onTick = new Timer(100, new ActionListener(){@Override public void actionPerformed(ActionEvent e){panel.update();}});
         
         window.add(panel);
-        window.addMouseListener(new MouseListener(){@Override public void mouseClicked(MouseEvent e){panel.shoot(e.getX(), e.getY());}@Override public void mousePressed(MouseEvent e){}@Override public void mouseReleased(MouseEvent e){}@Override public void mouseEntered(MouseEvent e){}@Override public void mouseExited(MouseEvent e){}});
+        window.addMouseListener(new MouseListener(){@Override public void mouseClicked(MouseEvent e){panel.shoot(e.getX()-13, e.getY()-35);}@Override public void mousePressed(MouseEvent e){}@Override public void mouseReleased(MouseEvent e){}@Override public void mouseEntered(MouseEvent e){}@Override public void mouseExited(MouseEvent e){}});
 
         window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        window.setSize(800, 600);
+        window.setSize(1000, 800);
         window.setLocationRelativeTo(null);
+        window.setResizable(false);
         window.setVisible(true);
 
         panel.setup();
