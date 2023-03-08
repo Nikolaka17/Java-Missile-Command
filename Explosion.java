5828e3c64083ecf151e6f517113b057a43bb9627
Nikolaka17 <92060768+Nikolaka17@users.noreply.github.com>
2023-03-08 15:25:22 -0600

    Update Explosion.java

--- Explosion.java
+++ Explosion.java
@@ -3,11 +3,11 @@ import java.awt.geom.Ellipse2D;
 public class Explosion extends Ellipse2D.Double{
     private int iterNum = 0;
     
-    public Explosion(int x, int y, int r){
-        this.x = x - r;
-        this.y = y - r;
-        this.height = r + r;
-        this.width = r + r;
+    public Explosion(int x, int y, double r){
+        this.x = x - (r/2);
+        this.y = y - (r/2);
+        this.width = r;
+        this.height = r;
     }
 
     public boolean grow(){
