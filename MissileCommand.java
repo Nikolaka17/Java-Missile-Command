44b736059acd09c97176b2c130c8010cea413f36
Nikolaka17 <92060768+Nikolaka17@users.noreply.github.com>
2023-03-08 20:27:45 -0600

    Update MissileCommand.java

--- MissileCommand.java
+++ MissileCommand.java
@@ -2,15 +2,36 @@ import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 import java.awt.event.MouseListener;
 import java.awt.event.MouseEvent;
+import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 import javax.swing.Timer;
+import javax.swing.JOptionPane;
 
 public class MissileCommand {
     
     public static void main(String[] args){
         JFrame window = new JFrame();
         MCWindow panel = new MCWindow();
-        Timer onTick = new Timer(100, new ActionListener(){@Override public void actionPerformed(ActionEvent e){panel.update();}});
+        Timer onTick = new Timer(10, new ActionListener(){@Override public void actionPerformed(ActionEvent e){panel.update();}});
+        Timer enemySpawner = new Timer(1000, new ActionListener(){@Override public void actionPerformed(ActionEvent e){panel.spawnEnemy();}});
+        Timer winChecker = new Timer(1, new ActionListener(){
+            @Override
+            public void actionPerformed(ActionEvent e){
+                if(panel.gameOver()){
+                    onTick.stop();
+                    enemySpawner.stop();
+                    panel.pause();
+                    int choice = JOptionPane.showConfirmDialog(window, "Congrats you scored: " + panel.getScore() + "\n\nRestart?", "Game Over", JOptionPane.YES_NO_OPTION, JOptionPane.INFORMATION_MESSAGE, new ImageIcon("Icon.png"));
+                    if(choice == JOptionPane.YES_OPTION){
+                        panel.setup();
+                        onTick.start();
+                        enemySpawner.start();
+                    }else{
+                        System.exit(0);
+                    }
+                }
+            }
+        });
         
         window.add(panel);
         window.addMouseListener(new MouseListener(){@Override public void mouseClicked(MouseEvent e){panel.shoot(e.getX()-13, e.getY()-35);}@Override public void mousePressed(MouseEvent e){}@Override public void mouseReleased(MouseEvent e){}@Override public void mouseEntered(MouseEvent e){}@Override public void mouseExited(MouseEvent e){}});
@@ -23,6 +44,8 @@ public class MissileCommand {
 
         panel.setup();
         onTick.start();
+        winChecker.start();
+        enemySpawner.start();
     }
 }
 
