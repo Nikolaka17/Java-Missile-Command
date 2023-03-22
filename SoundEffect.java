5f5cba683439f9a19845f643b9df56e5bf40d26a
Nikolaka17 <92060768+Nikolaka17@users.noreply.github.com>
2023-03-22 13:40:39 -0500

    Update SoundEffect.java

--- SoundEffect.java
+++ SoundEffect.java
@@ -3,13 +3,24 @@ import javax.sound.sampled.AudioInputStream;
 import javax.sound.sampled.AudioSystem;
 import java.io.File;
 
+/**
+ * A class that holds a sound byte to play
+ */
 public class SoundEffect{
     Clip clip;
 
+    /**
+     * Constructor to make a sound effect with a specified file
+     * @param path The file to use as the sound
+     */
     public SoundEffect(String path){
         setFile(path);
     }
 
+    /**
+     * Switches the sound file for the sound effect
+     * @param path The new file to use as the sound
+     */
     public void setFile(String path){
         try{
             File file = new File(path);
@@ -21,6 +32,9 @@ public class SoundEffect{
         }
     }
 
+    /**
+     * Plays the sound effect
+     */
     public void play(){
         clip.setFramePosition(0);
         clip.start();
